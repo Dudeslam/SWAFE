8530faa6ae249c7459ad3ce2c492cc7f05e95fba
Vinh Trung <Trung_thai@live.dk>
2021-09-13 16:41:42 +0200

    updated stuff

--- exercise03/src/app/app.module.ts
+++ exercise03/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from './http-client/http-client.module';
+
+
+const routes: Routes = [
+  {path: "/HttpClient", component: HttpClientModule}
+];
 
 @NgModule({
   declarations: [
--- exercise03/src/app/client.service.ts
+++ exercise03/src/app/client.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@angular/core';
 
+export interface node{
+  "id": 2,
+  "picture": "https://robohash.org/idetet.jpg?size=150x150&set=set1",
+  "first_name": "Vance",
+  "last_name": "Jime",
+  "email": "vjime1@deviantart.com",
+  "gender": "Male",
+  "wallet": "1KQmmY7gDQxjoaVpyqzU6MtNv67bxhrw8T" 
+}
+
 @Injectable({
   providedIn: 'root'
 })
