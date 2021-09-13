9799cf75820d3788ddb6ecbdf3553efc6c8ac121
Vinh Trung <Trung_thai@live.dk>
2021-09-13 16:37:32 +0200

    Almost done with exercise02 Started on exercise03

---
+++ exercise02/src/app/user/user-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { ListComponent } from './list/list.component';
+
+const routes: Routes = [
+  {path: "/DetailsComponent", component: DetailsComponent},
+  {path: "/ListComponent", component: ListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
