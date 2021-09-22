8256fdfdb821ada09b13813b71ea38717ae9b039
Vinh Trung <Trung_thai@live.dk>
2021-09-22 11:05:04 +0200

    Added interface and some routing

--- hand-in/hand-in01/src/app/app-routing.module.ts
+++ hand-in/hand-in01/src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
+import { CreditCardAddComponent } from './credit-card/credit-card-add/credit-card-add.component';
+import { TransactionModule } from './transaction/transaction.module';
+import { TransactionOverviewComponent } from './transaction/transaction-overview/transaction-overview.component';
 
 
 const routes: Routes = [
@@ -12,6 +15,14 @@ const routes: Routes = [
   {
     path: 'NavBar',
     component: NavBarComponent
+  },
+  {
+    path: 'AddCards',
+    component: CreditCardAddComponent
+  },
+  {
+    path: 'Transact',
+    component: TransactionOverviewComponent
   }
 
 ];
--- hand-in/hand-in01/src/app/credit-card/credit-card-list-item/credit-card-list-item.component.ts
+++ hand-in/hand-in01/src/app/credit-card/credit-card-list-item/credit-card-list-item.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface CreditCardDetails{
+  cardNumber: number,
+  cardHolder_name: string,
+  csc_code: number,
+  expiration_date_month: Date,
+  expiration_date_year: Date,
+  issuer: string
+}
+
+
 @Component({
   selector: 'app-credit-card-list-item',
   templateUrl: './credit-card-list-item.component.html',
@@ -8,8 +18,10 @@ import { Component, OnInit } from '@angular/core';
 export class CreditCardListItemComponent implements OnInit {
 
   constructor() { }
+  
 
   ngOnInit(): void {
   }
 
+  
 }
